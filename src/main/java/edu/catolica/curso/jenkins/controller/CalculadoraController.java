ad5901edaf712c7fce5418bd2300618d036f72c3
lucciano01 <lucciano01@hotmail.com>
2023-12-02 19:40:57 -0300

    ajustes no projetopara gerar o arquivo .war

--- src/main/java/edu/catolica/curso/jenkins/controller/CalculadoraController.java
+++ src/main/java/edu/catolica/curso/jenkins/controller/CalculadoraController.java
@@ -2,14 +2,15 @@ package edu.catolica.curso.jenkins.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/calculadora")
+@RestController
 public class CalculadoraController {
 
     @GetMapping("/ola")
     public String hello(){
-        return "Seja bem vindo(a) ao Mini Curso de Jenkins!";
+        return "Seja bem vindo(a) ao Mini Curso de Jenkins";
     }
 
     @GetMapping("/somar/{a}/{b}")
