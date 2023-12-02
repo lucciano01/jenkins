a2b84cd9ebffde09a54dfbd5af3f0459dbff5785
lucciano01 <lucciano01@hotmail.com>
2023-12-02 15:03:35 -0300

    ajustes no projetopara gerar o arquivo .war

--- src/main/java/edu/catolica/curso/jenkins/CursoJenkinsApplication.java
+++ src/main/java/edu/catolica/curso/jenkins/CursoJenkinsApplication.java
@@ -7,13 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class CursoJenkinsApplication extends SpringBootServletInitializer {
-
-	@GetMapping("/jenkins")
-	public String init(){
-		return "Bem vindo ao Mini Curso de Jenkins!!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoJenkinsApplication.class, args);
