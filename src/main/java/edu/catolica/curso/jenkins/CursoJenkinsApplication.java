3e28a8fa6591725c79f37963e196f5777ddc52ca
lucciano01 <lucciano01@hotmail.com>
2023-12-02 17:01:47 -0300

    ajustes no projetopara gerar o arquivo .war

--- src/main/java/edu/catolica/curso/jenkins/CursoJenkinsApplication.java
+++ src/main/java/edu/catolica/curso/jenkins/CursoJenkinsApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class CursoJenkinsApplication extends SpringBootServletInitializer {
+public class CursoJenkinsApplication extends SpringBootServletInitializer{
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -18,5 +16,8 @@ public class CursoJenkinsApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(CursoJenkinsApplication.class, args);
 	}
+
+
+
 
 }
--- src/main/java/edu/catolica/curso/jenkins/controller/CalculadoraController.java
+++ src/main/java/edu/catolica/curso/jenkins/controller/CalculadoraController.java
@@ -2,15 +2,14 @@ package edu.catolica.curso.jenkins.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/calculadora")
 public class CalculadoraController {
 
     @GetMapping("/ola")
     public String hello(){
-        return "Seja bem vindo ao Mini Curso de Jenkins!";
+        return "Seja bem vindo(a) ao Mini Curso de Jenkins!";
     }
 
     @GetMapping("/somar/{a}/{b}")
