edb6bf7fb56316ae7c2534af51099f0508ea3702
lucciano01 <lucciano01@hotmail.com>
2023-12-02 15:27:00 -0300

    ajustes no projetopara gerar o arquivo .war

--- src/main/java/edu/catolica/curso/jenkins/CursoJenkinsApplication.java
+++ src/main/java/edu/catolica/curso/jenkins/CursoJenkinsApplication.java
@@ -2,12 +2,18 @@ package edu.catolica.curso.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CursoJenkinsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CursoJenkinsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoJenkinsApplication.class, args);
